8967ea500f0c870e2efebd8b4b4f94c097b0b9f9
吴仙杰 <wuxianjiezh@gmail.com>
2022-09-19 15:53:43 +0800

    优化前端路由 404 时的 HTTP 响应状态码

--- src/main/java/net/wuxianjie/springrestapi/shared/spa/NotFoundController.java
+++ src/main/java/net/wuxianjie/springrestapi/shared/spa/NotFoundController.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
 public class NotFoundController {
 
   @RequestMapping(value = "/404", produces = MediaType.TEXT_HTML_VALUE)
-  public ModelAndView errorHtml() {
+  public ModelAndView errorHtml(final HttpServletResponse response) {
+    // 单页页应用程序 404 应由前端路由自行提供，后端返回 200 即可
+    response.setStatus(HttpStatus.OK.value());
     // Spring Boot 默认会将 `src/main/resources/static/` 中的内容作为静态资源提供
     //noinspection SpringMVCViewInspection
     return new ModelAndView("index.html");
