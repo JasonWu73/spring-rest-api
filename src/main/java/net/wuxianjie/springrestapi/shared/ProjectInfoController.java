90896198551999018c95a748d3b230d7dd6295e3
吴仙杰 <wuxianjiezh@gmail.com>
2022-10-09 13:55:14 +0800

    添加版本信息 API

--- src/main/java/net/wuxianjie/springrestapi/shared/ProjectInfoController.java
+++ src/main/java/net/wuxianjie/springrestapi/shared/ProjectInfoController.java
@@ -1,21 +1,30 @@
 package net.wuxianjie.springrestapi.shared;
 
+import net.wuxianjie.springrestapi.shared.util.StrUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 
 @RestController
 public class ProjectInfoController {
 
+  /**
+   * 获取版本信息.
+   *
+   * @return <pre>{@code
+   * {
+   *   "version": "v1.0.0", // 版本号
+   *   "machineCode": "63342D34312D31652D62322D31322D3034" // 机器码
+   * }
+   * }</pre>
+   */
   @GetMapping("/api/v1/version")
   public ResponseEntity<LinkedHashMap<String, Object>> getVersion() {
     return ResponseEntity.ok(new LinkedHashMap<>() {{
       put("version", "v1.0.0");
-      put("built", LocalDateTime.now());
+      put("machineCode", StrUtils.getMachineCode());
     }});
   }
-
 }
