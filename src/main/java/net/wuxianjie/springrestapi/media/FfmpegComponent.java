d94b8f245c1504e5a8805ab98f6cd259575fdf0b
吴仙杰 <wuxianjiezh@gmail.com>
2022-10-13 17:36:47 +0800

    修复调用外部程序可能出错的阻塞问题

--- src/main/java/net/wuxianjie/springrestapi/media/FfmpegComponent.java
+++ src/main/java/net/wuxianjie/springrestapi/media/FfmpegComponent.java
@@ -1,12 +1,11 @@
 package net.wuxianjie.springrestapi.media;
 
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.core.util.StrUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -14,6 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class FfmpegComponent {
 
   public void extractMp3(final String sourceFilePath, final String targetFilePath) {
+
+    // 提取音频时偶尔会无法退出 FFmpeg 进程, 故采用异步执行, 且超时后销毁进程
     ThreadUtil.execAsync(() -> {
       // `ffmpeg -y -i trial.mp4 trial.mp3`
       final String command = StrUtil.format(
@@ -22,17 +23,26 @@ public class FfmpegComponent {
         "file:" + sourceFilePath,
         "file:" + targetFilePath
       );
-
-      try {
-        final Process process = Runtime.getRuntime().exec(command);
-        final boolean isExited = process.waitFor(10, TimeUnit.MINUTES);
-        if (!isExited) {
-          process.destroy();
-          log.error("音频可能提取失败 [{}]", command);
-        }
-      } catch (Exception e) {
-        log.error("音频提取失败 [{}]", command, e);
-      }
+      final Process ffmpeg = RuntimeUtil.exec(command);
+      waitForTimeout(ffmpeg, command);
     });
+  }
+
+  private void waitForTimeout(final Process process, final String command) {
+    // 5 分钟
+    long timeoutMillis = 300_000;
+    do {
+      // 每 30 秒检查一次
+      final int sleepMillis = 30_000;
+      ThreadUtil.sleep(sleepMillis);
+      timeoutMillis -= sleepMillis;
+
+      if (!process.isAlive()) {
+        return;
+      }
+    } while (timeoutMillis > 0);
+
+    RuntimeUtil.destroy(process);
+    log.error("音频提取超时 [{}]", command);
   }
 }
