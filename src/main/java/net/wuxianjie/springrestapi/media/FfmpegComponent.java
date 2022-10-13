3c2f9be14496f3016b21868cedc10ae3ba9235c5
吴仙杰 <wuxianjiezh@gmail.com>
2022-10-13 13:11:49 +0800

    解决提取音频会出现阻塞的问题

--- src/main/java/net/wuxianjie/springrestapi/media/FfmpegComponent.java
+++ src/main/java/net/wuxianjie/springrestapi/media/FfmpegComponent.java
@@ -1,32 +1,38 @@
 package net.wuxianjie.springrestapi.media;
 
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.StrUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FfmpegComponent {
 
-  public void extractMp3(final String sourceAbsoluteFilePath, final String targetMp3AbsoluteFilePath) {
-    // `ffmpeg -y -i trial.mp4 trial.mp3`
-    final String command = StrUtil.format(
-      "{} -y -i {} {}",
-      "ffmpeg",
-      "file:" + sourceAbsoluteFilePath,
-      "file:" + targetMp3AbsoluteFilePath
-    );
-    try {
-      final Process process = Runtime.getRuntime().exec(command);
-      final int signal = process.waitFor();
+  public void extractMp3(final String sourceFilePath, final String targetFilePath) {
+    ThreadUtil.execAsync(() -> {
+      // `ffmpeg -y -i trial.mp4 trial.mp3`
+      final String command = StrUtil.format(
+        "{} -y -i {} {}",
+        "ffmpeg",
+        "file:" + sourceFilePath,
+        "file:" + targetFilePath
+      );
 
-      if (signal != 0) {
-        log.error("音频可能提取失败 [{}], Exit Code: {}", command, signal);
+      try {
+        final Process process = Runtime.getRuntime().exec(command);
+        final boolean isExited = process.waitFor(10, TimeUnit.MINUTES);
+        if (!isExited) {
+          process.destroy();
+          log.error("音频可能提取失败 [{}]", command);
+        }
+      } catch (Exception e) {
+        log.error("音频提取失败 [{}]", command, e);
       }
-    } catch (Exception e) {
-      log.error("音频提取失败 [{}]", command, e);
-    }
+    });
   }
 }
