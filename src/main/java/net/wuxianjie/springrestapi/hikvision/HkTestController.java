710e827a2e6533cae3337c14db89aa2681da4e92
吴仙杰 <wuxianjiezh@gmail.com>
2022-10-24 09:34:39 +0800

    重命名类

--- src/main/java/net/wuxianjie/springrestapi/hikvision/HkTestController.java
+++ src/main/java/net/wuxianjie/springrestapi/hikvision/HkTestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class HkTestController {
 
   private final HkSdkComponent hkSdk;
 
