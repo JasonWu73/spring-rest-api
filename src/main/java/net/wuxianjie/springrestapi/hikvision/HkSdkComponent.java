80b5a1116b252b3f301df2caf0c9ef577778ac68
吴仙杰 <wuxianjiezh@gmail.com>
2022-11-09 21:38:32 +0800

    增加海康 SDK 交互

--- src/main/java/net/wuxianjie/springrestapi/hikvision/HkSdkComponent.java
+++ src/main/java/net/wuxianjie/springrestapi/hikvision/HkSdkComponent.java
@@ -3,6 +3,9 @@ package net.wuxianjie.springrestapi.hikvision;
 import cn.hutool.core.util.StrUtil;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -19,33 +22,73 @@ import static net.wuxianjie.springrestapi.hikvision.HkSdkConfiguration.HC_NET_SD
 public class HkSdkComponent {
 
   /**
-   * 获取设备序列号.
+   * 获取主机状态.
    *
-   * @return 设备序列号
+   * @param deviceIp 设备 IP
+   * @param devicePort 设备端口号
+   * @param username 设备登录用户名
+   * @param password 设备登录密码
+   * @param channel 通道号
+   * @return 主机状态
    */
-  public Optional<String> getSn(
+  public NvrStatus getNvrStatus(
     final String deviceIp,
     final int devicePort,
     final String username,
-    final String password
+    final String password,
+    final int channel
   ) {
     if (HC_NET_SDK == null) throw new RuntimeException("海康 SDK 初始化失败");
 
-    // 登录设备, 每一台设备分别登录; 登录句柄是唯一的, 可以区分设备
-    final HCNetSDK.NET_DVR_USER_LOGIN_INFO loginInfo = new HCNetSDK.NET_DVR_USER_LOGIN_INFO();// 设备登录信息
+    // 在线状态
     final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo = new HCNetSDK.NET_DVR_DEVICEINFO_V40();// 设备信息
-    fillInLoginInfo(deviceIp, (short) devicePort, username, password, loginInfo);
-    final int userHandle = HC_NET_SDK.NET_DVR_Login_V40(loginInfo, deviceInfo); // 用户句柄
-    if (userHandle == -1) {
-      log.error("设备登录失败 [错误码={}]", HC_NET_SDK.NET_DVR_GetLastError());
-      return Optional.empty();
-    } else {
-      final String sn = new String(deviceInfo.struDeviceV30.sSerialNumber).trim();
-      log.warn("设备登录成功 [IP={};SN={}]", deviceIp, sn);
+    final int userHandle = login(deviceIp, devicePort, username, password, deviceInfo);
+    if (userHandle == -1) return new NvrStatus(false, null, null, null, null);
 
-      // 注销
+    try {
+      final NvrStatus status = new NvrStatus(true, null, null, null, null);
+      // 录像状态
+      final HCNetSDK.NET_DVR_WORKSTATE_V30 workStatus = new HCNetSDK.NET_DVR_WORKSTATE_V30();
+      workStatus.write();
+      if (!HC_NET_SDK.NET_DVR_GetDVRWorkState_V30(userHandle, workStatus)) {
+        log.warn("海康 SDK 获取设备状态失败");
+        return status;
+      }
+      workStatus.read();
+
+      // 通道的状态, 从前往后顺序排列
+      final byte startDigitalChannel = deviceInfo.struDeviceV30.byStartDChan; // 数字通道起始通道号
+      // for (int i = 0; i < workStatus.struChanStatic.length; i++) {
+      // log.warn("{}. 录像状态: {}", i, workStatus.struChanStatic[i].byRecordStatic);
+      // }
+      final int channelIndex = (startDigitalChannel - 1) + (channel - 1);
+      if (channelIndex <= workStatus.struChanStatic.length) {
+        // 通道是否在录像, 0: 不录像, 1: 录像
+        status.setRecordStatus(workStatus.struChanStatic[channelIndex].byRecordStatic == 1);
+      } else {
+        log.warn("未找到通道的状态 [channel={}]", channel);
+      }
+
+      // 硬盘状态, 只要有一个不正常就认为硬盘异常
+      boolean diskStatus = true;
+      // 硬盘状态, 一次最多只能获取 33 个硬盘信息
+      for (int i = 0; i < workStatus.struHardDiskStatic.length; i++) {
+        // log.warn("{}. 硬盘状态: {}", i, workStatus.struHardDiskStatic[i].dwHardDiskStatic);
+        // 硬盘状态, 0: 正常
+        if (workStatus.struHardDiskStatic[i].dwHardDiskStatic != 0) {
+          diskStatus = false;
+          break;
+        }
+      }
+      status.setDiskStatus(diskStatus);
+
+      // 音视频状态, 与通道是否存在为准即可, 即当通道状态不存在时, 才设置为 null, 否则都为 true
+      status.setAudioStatus(status.getRecordStatus() == null ? null : true);
+      status.setVideoStatus(status.getRecordStatus() == null ? null : true);
+
+      return status;
+    } finally {
       logout(userHandle);
-      return Optional.of(sn);
     }
   }
 
@@ -83,7 +126,8 @@ public class HkSdkComponent {
 
     if (null == contents || contents.length == 0) return;
 
-    final int userHandle = login(deviceIp, devicePort, username, password);
+    final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo = new HCNetSDK.NET_DVR_DEVICEINFO_V40();// 设备信息
+    final int userHandle = login(deviceIp, devicePort, username, password, deviceInfo);
     if (userHandle == -1) return;
 
     try {
@@ -137,7 +181,8 @@ public class HkSdkComponent {
   ) {
     if (HC_NET_SDK == null) return;
 
-    final int userHandle = login(deviceIp, devicePort, username, password);
+    final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo = new HCNetSDK.NET_DVR_DEVICEINFO_V40();// 设备信息
+    final int userHandle = login(deviceIp, devicePort, username, password, deviceInfo);
     if (userHandle == -1) return;
 
     try {
@@ -156,24 +201,77 @@ public class HkSdkComponent {
     }
   }
 
-  private int login(final String deviceIp, final int devicePort, final String username, final String password) {
+  /**
+   * 获取设备序列号.
+   *
+   * @param deviceIp 设备 IP
+   * @param devicePort 设备端口号
+   * @param username 设备登录用户名
+   * @param password 设备登录密码
+   * @return 设备序列号
+   */
+  public Optional<String> getSn(
+    final String deviceIp,
+    final int devicePort,
+    final String username,
+    final String password
+  ) {
+    if (HC_NET_SDK == null) throw new RuntimeException("海康 SDK 初始化失败");
+
     // 登录设备, 每一台设备分别登录; 登录句柄是唯一的, 可以区分设备
     final HCNetSDK.NET_DVR_USER_LOGIN_INFO loginInfo = new HCNetSDK.NET_DVR_USER_LOGIN_INFO();// 设备登录信息
     final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo = new HCNetSDK.NET_DVR_DEVICEINFO_V40();// 设备信息
     fillInLoginInfo(deviceIp, (short) devicePort, username, password, loginInfo);
     final int userHandle = HC_NET_SDK.NET_DVR_Login_V40(loginInfo, deviceInfo); // 用户句柄
     if (userHandle == -1) {
-      log.error("设备登录失败 [错误码={}]", HC_NET_SDK.NET_DVR_GetLastError());
+      log.error("设备登录失败 [IP={};错误码={}]", deviceIp, HC_NET_SDK.NET_DVR_GetLastError());
+      return Optional.empty();
+    } else {
+      deviceInfo.read();
+      logDeviceInfo(deviceIp, deviceInfo);
+
+      // 注销
+      logout(userHandle);
+      return Optional.of(new String(deviceInfo.struDeviceV30.sSerialNumber).trim());
+    }
+  }
+
+  private int login(
+    final String deviceIp,
+    final int devicePort,
+    final String username,
+    final String password,
+    final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo
+  ) {
+    // 登录设备, 每一台设备分别登录; 登录句柄是唯一的, 可以区分设备
+    final HCNetSDK.NET_DVR_USER_LOGIN_INFO loginInfo = new HCNetSDK.NET_DVR_USER_LOGIN_INFO();// 设备登录信息
+    fillInLoginInfo(deviceIp, (short) devicePort, username, password, loginInfo);
+    final int userHandle = HC_NET_SDK.NET_DVR_Login_V40(loginInfo, deviceInfo); // 用户句柄
+    if (userHandle == -1) {
+      log.error("设备登录失败 [IP={};错误码={}]", deviceIp, HC_NET_SDK.NET_DVR_GetLastError());
       return userHandle;
     } else {
-      log.warn(
-        "设备登录成功 [IP={};SN={}]",
-        deviceIp,
-        new String(deviceInfo.struDeviceV30.sSerialNumber).trim()
-      );
       deviceInfo.read();
+      logDeviceInfo(deviceIp, deviceInfo);
     }
     return userHandle;
+  }
+
+  private static void logDeviceInfo(final String deviceIp, final HCNetSDK.NET_DVR_DEVICEINFO_V40 deviceInfo) {
+    final String sn = new String(deviceInfo.struDeviceV30.sSerialNumber).trim();
+    final byte channelNums = deviceInfo.struDeviceV30.byChanNum;
+    final byte startChannel = deviceInfo.struDeviceV30.byStartChan;
+    final int ipChannelNums = deviceInfo.struDeviceV30.byIPChanNum + deviceInfo.struDeviceV30.byHighDChanNum * 256;
+    final byte startDigitalChannel = deviceInfo.struDeviceV30.byStartDChan;
+    log.warn(
+      "设备登录成功 [IP={};SN={};模拟通道个数={};模拟通道起始通道号={};设备支持的最大 IP 通道数={};数字通道起始通道号={}]",
+      deviceIp,
+      sn,
+      channelNums,
+      startChannel,
+      ipChannelNums,
+      startDigitalChannel
+    );
   }
 
   private static void fillInLoginInfo(
@@ -206,20 +304,22 @@ public class HkSdkComponent {
     // 获取叠加字符参数 NET_DVR_SHOWSTRING_V30
     final HCNetSDK.NET_DVR_SHOWSTRING_V30 osdCfg = new HCNetSDK.NET_DVR_SHOWSTRING_V30();
     osdCfg.write();
-    // 获取网络参数 NET_DVR_GetDVRConfig
-    final Pointer osdPointer = osdCfg.getPointer(); // 接收数据的缓冲指针
-    final IntByReference bytesReturned = new IntByReference(0); // 获取 OSD 配置参数
+
+    // 获取通道参数 NET_DVR_GetDVRConfig
+    final Pointer pointer = osdCfg.getPointer(); // 接收数据的缓冲指针
+    final IntByReference ref = new IntByReference(0); // 获取 OSD 配置参数
     if (!HC_NET_SDK.NET_DVR_GetDVRConfig(
       userHandle,
       HCNetSDK.NET_DVR_GET_SHOWSTRING_V30,
       channel,
-      osdPointer,
+      pointer,
       osdCfg.size(),
-      bytesReturned
+      ref
     )) {
       log.error("海康 SDK 获取叠加字符参数失败 [错误码={}]", HC_NET_SDK.NET_DVR_GetLastError());
       return null;
     }
+
     osdCfg.read();
     // log.warn("debug osdCfg: {} -> {}", osdCfg, osdCfg.struStringInfo);
     return osdCfg;
@@ -237,5 +337,17 @@ public class HkSdkComponent {
     )) {
       log.error("海康 SDK 设置叠加字符参数失败 [错误码={}]", HC_NET_SDK.NET_DVR_GetLastError());
     }
+  }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class NvrStatus {
+
+    private Boolean online; // 网络状态
+    private Boolean recordStatus; // 录像状态
+    private Boolean diskStatus; // 硬盘状态
+    private Boolean audioStatus; // 音频状态
+    private Boolean videoStatus; // 视频状态
   }
 }
